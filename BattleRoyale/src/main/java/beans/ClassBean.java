16fbb3f78810b1a4601e01e88309d47735a96635
Victor Costa <victorsc_rs@hotmail.com>
2017-12-05 18:16:54 -0200

    paginas com individuos

--- BattleRoyale/src/main/java/beans/ClassBean.java
+++ BattleRoyale/src/main/java/beans/ClassBean.java
@@ -5,6 +5,8 @@
  */
 package beans;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import ontology.OntoClass;
 import ontology.OntoIndividual;
@@ -17,6 +19,10 @@ import ontology.OntoIndividual;
 public class ClassBean {
 
     private String nome;
+    private List<OntoIndividual> acoes;
+    private List<OntoIndividual> condicoes;
+    private List<OntoIndividual> mapas;
+    private List<OntoIndividual> personagens;
 
     public ClassBean() {
     }
@@ -31,6 +37,27 @@ public class ClassBean {
         this.nome = nome;
     }
     
+    public List<OntoIndividual> getAcoes(){
+        OntoClass ontoClass = new OntoClass("http://webprotege.stanford.edu/R9fbSoZ8jtVpMIREteazOyc");
+        this.acoes = ontoClass.getIndividuals();
+        return this.acoes;
+    }
     
+    public List<OntoIndividual> getCondicoes(){
+        OntoClass ontoClass = new OntoClass("http://webprotege.stanford.edu/RCnktEYsOF1yGqvhWobfdlK");
+        this.condicoes = ontoClass.getIndividuals();
+        return this.condicoes;
+    }
     
+    public List<OntoIndividual> getMapas(){
+        OntoClass ontoClass = new OntoClass("http://webprotege.stanford.edu/R7pmm8WpDKzV6uxkTQX8N2r");
+        this.mapas = ontoClass.getIndividuals();
+        return this.mapas;
+    }
+    
+    public List<OntoIndividual> getPersonagens(){
+        OntoClass ontoClass = new OntoClass("http://webprotege.stanford.edu/R9qKrbUMVakzRjJlgJQcVor");
+        this.personagens = ontoClass.getIndividuals();
+        return this.personagens;
+    }
 }
